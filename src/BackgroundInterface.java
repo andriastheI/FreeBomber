35a08902d60185f574b0ecc804af7827c987161c
mguzelocak <mrtcnguzelocak@gmail.com>
2025-02-21 09:01:01 -0700

    Created Interface files for background and keyhandler according to the branch Andrias in github and updated a JackBomber class

---
+++ src/BackgroundInterface.java
@@ -0,0 +1,17 @@
+package src;
+import java.awt.*;
+
+public interface BackgroundInterface {
+
+        int FPS = 60;
+        public int TILE_SIZE = 46;
+        int SCREEN_COLS = 18;
+        int SCREEN_ROWS = 14;
+        int SCREEN_WIDTH = SCREEN_COLS * TILE_SIZE;
+        int SCREEN_HEIGHT = SCREEN_ROWS * TILE_SIZE;
+
+        void startGameThread();
+        void run();
+        void update();
+        void paintComponent(Graphics g);
+}
--- src/JackBomber.java
+++ src/JackBomber.java
@@ -1,5 +1,36 @@
 package src;
 
-public class JackBomber extends Character {
+import java.awt.*;
 
+public class JackBomber extends Character {
+    BackgroundInterface background;
+    KeyHandlerInterface keyHandler;
+
+    public void Player(BackgroundInterface bg, KeyHandlerInterface kh) {
+        this.background = bg;
+        this.keyHandler = kh;
+        setDefaultValues();
+    }
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+    }
+
+    public void update() {
+        if(keyHandler.upDirection == true){
+            y -= speed;
+        } else if(keyHandler.downDirection == true){
+            y += speed;
+        } else if(keyHandler.leftDirection == true){
+            x -= playerSp;
+        } else if(keyHandler.rightDirection == true){
+            x += playerSp;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g2.setColor(Color.WHITE);
+        g2.fillRect(x, y, background.TILE_SIZE, background.TILE_SIZE);
+    }
 }
