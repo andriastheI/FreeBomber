7ead8b30b88d6fd217abb4892715a4e46b0d2703
mguzelocak <mrtcnguzelocak@gmail.com>
2025-02-23 21:35:21 -0700

    added character.java and jackbomber.class

--- src/JackBomber.java
+++ src/JackBomber.java
@@ -1,16 +1,20 @@
 package src;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class JackBomber extends Character {
-    BackgroundInterface background;
-    KeyHandlerInterface keyHandler;
+    Background background;
+    KeyHandler keyHandler;
 
-    public void Player(BackgroundInterface bg, KeyHandlerInterface kh) {
+    public void Player(Background bg, KeyHandler kh) {
         this.background = bg;
         this.keyHandler = kh;
         setDefaultValues();
     }
+
     public void setDefaultValues() {
         x = 100;
         y = 100;
@@ -18,19 +22,50 @@ public class JackBomber extends Character {
     }
 
     public void update() {
-        if(keyHandler.upDirection == true){
+        if(keyHandler.upDirection){
             y -= speed;
-        } else if(keyHandler.downDirection == true){
+        } else if(keyHandler.downDirection){
             y += speed;
-        } else if(keyHandler.leftDirection == true){
-            x -= playerSp;
-        } else if(keyHandler.rightDirection == true){
-            x += playerSp;
+        } else if(keyHandler.leftDirection){
+            x -= speed;
+        } else if(keyHandler.rightDirection){
+            x += speed;
         }
     }
 
     public void draw(Graphics g) {
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, background.TILE_SIZE, background.TILE_SIZE);
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, background.tileSize, background.tileSize);
+    }
+
+    private void crop() {
+        try {
+            File imageFile = new File("src/storage/player/champ1.png");
+            BufferedImage img = ImageIO.read(imageFile);
+            String[] naming = { "up", "down", "left", "right"};
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    int cropX = j * 32;
+                    int cropY = i * 48;
+                    int cropWidth = 32;
+                    int cropHeight = 48;
+                    BufferedImage croppedImage = img.getSubimage(cropX, cropY, cropWidth, cropHeight);
+                    System.out.println("cropX = " + cropX + " cropY = " + cropY + " cropWidth = " + cropWidth + " cropHeight = " + cropHeight + "");
+                    File outputfile = new File("src/storage/player/champ1_cropped_" + naming[i] + "_" + (j + 1) + ".png");
+                    ImageIO.write(croppedImage, "png", outputfile);
+                    System.out.println("Done");
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new File("storage/player/champ1.png").getAbsolutePath());
+        JackBomber jackBomber = new JackBomber();
+        jackBomber.crop();
     }
 }
